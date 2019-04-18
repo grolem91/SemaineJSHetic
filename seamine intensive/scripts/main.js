ae6adb3739c5ed48705b33706716b338d77ce52b
grolem91 <44402718+grolem91@users.noreply.github.com>
2019-04-18 14:10:58 +0200

    Add files via upload

---
+++ seamine intensive/scripts/main.js
@@ -0,0 +1,124 @@
+/* exo1
+let number1 = parseInt(window.prompt('renseigner un nombre'))
+if(number1<0)
+{
+  document.write('c est negatif')
+}
+else
+{
+  document.write('positif')
+}
+*/
+/*
+let number1 = parseInt(window.prompt('renseigner un nombre'))
+let number2 = parseInt(window.prompt('un dexieme nombre'))
+if(number1 * number2<0)
+{
+  document.write(positif)
+}
+/*
+exercice 5
+*let number1 = parseInt(window.prompt('quel est le premier nombre?',  ))
+let number2 = parseInt(window.prompt('quel est le deuxiéme nombre?',  ))
+let number3 = parseInt(window.prompt('quel est le troisiéme nombre?',  ))
+if (number1 < number2 && number2 < number3)
+  document.write('1er nombre 2éme nombre 3éme nombre')
+if (number1 > number2 && number2 < number3 && number1 < number3)
+  document.write('3éme nombres 1er nombre 2éme nombre')
+if (number1 > number2 && number2 > number3)
+  document.write('3éme nombre 2éme nombre 1er nombre')
+if (number3 > number2 && number3 > number1 && number2 > number 1)
+    document.write('1er nombre 3éme nombre 2éme nombre')
+if (number1 > number3 && number3 > number2)
+    document.write('1er nombre 3éme nombre 2éme nombre')
+if (number2 > number3 && number3 > number1)
+    document.write('1er nombre 3éme nombre 2éme nombre')
+if (number3 > number2 && number2 > number1)
+    document.write('1er nombre 3éme nombre 2éme nombre')
+if (number2 > number3 && number3 > number1)
+    document.write('1er nombre 3éme nombre 2éme nombre')
+/*
+exercice 3
+let number1 = parseInt(window.prompt('quel est le premier nombre?',  ))
+let number2 = parseInt(window.prompt('quel est le second nombre?',  ))
+let result = number1 * number2
+if (number1 > 0 && number2 > 0)
+  document.write('positif')
+
+if (number1 = 0 && number2 = 0)
+    document.write('est égale a 0')
+
+if (number1 < 0 && number2 > 0)
+  document.write('negatif')
+
+if (number1 > 0 && number2 < 0)
+  document.write('negatif')
+
+if (number1 < 0 && number2 < 0)
+  document.write('negatif')
+*/
+/*
+exercice 2
+let number1 = parseInt(window.prompt('quel est le premier nombre?',  ))
+let number2 = parseInt(window.prompt('quel est le second nombre?',  ))
+let result = number1 * number2
+if (number1 > 0 && number2 > 0)
+  document.write('positif')
+
+if (number1 < 0 && number2 > 0)
+  document.write('negatif')
+
+if (number1 > 0 && number2 < 0)
+  document.write('negatif')
+
+if (number1 < 0 && number2 < 0)
+  document.write('positif')
+
+
+
+/* exercice 4
+let number = parseInt(window.prompt('quel est votre age?'))
+if( number <= 3 )
+{
+  document.write('bebe')
+}
+if( number >=4 && number <=12)
+{
+  document.write('enfant')
+}
+if( number >=13 && number <=18)
+{
+  document.write('adolescent')
+}
+if( number >=19 && number <=25)
+{
+  document.write('etudiant')
+}
+if( number >=26 && number <=75)
+{
+  document.write('actif')
+}
+if( number >=76)
+{
+  document.write('retraité')
+}
+*/
+
+
+
+
+
+let number = parseInt(window.prompt('choisir un nombre entier entre 1 et 5'))
+if ( number>=2 ^ number<=5)
+document.write('gg poto')
+while (number<1  ^ number >5){
+let number = parseInt(window.prompt('réessaye'))
+ ( number>=2 ^ number<=5)
+document.write('gg poto')
+}
+
+
+
+/* exercice 2
+
+*/
